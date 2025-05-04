32f7e9c9647bffc27a1557eec17e563f2b040fed
Zlata <zlata.zenchenko@gmail.com>
2025-05-04 22:44:37 +0300

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+
+filename = "/content/skempi_v2.csv"
